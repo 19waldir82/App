711e95cda3c2722d35e44083c025847cca2bac0c
Waldir Aparecido da Silva <63414398+19waldir82@users.noreply.github.com>
2020-11-07 17:59:18 -0300

    07/11/20

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import Primeiro from './Componentes/primeiro'
+
+export default () => (
+    <View>
+        <Primeiro />
+    </View>
+)
