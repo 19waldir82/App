1b94e955b28bb1a0ce1d2c43cae05a91e63a6669
Waldir Aparecido da Silva <63414398+19waldir82@users.noreply.github.com>
2020-11-08 07:34:59 -0300

    08/11/20

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { View } from 'react-native'
 
 import Primeiro from './Componentes/primeiro'
+import Padrao, { Segundo, Terceiro } from './Componentes/Mult'
 
 export default () => (
     <View>
         <Primeiro />
+        <Padrao />
+        <Segundo />
+        <Terceiro />
     </View>
 )
---
+++ src/Componentes/Mult.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default () => {
+    return ( 
+        <Text>Padrão</Text> 
+    )
+}
+
+export const Segundo = () => {
+    return(
+        <Text>Segundo</Text>
+    )
+}
+
+export const Terceiro = () => {
+    return(
+        <Text>Terceiro</Text>
+    )
+}
